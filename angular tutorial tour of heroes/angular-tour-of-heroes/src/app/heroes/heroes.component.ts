dc739aef6913d19cd6ebe45f077dab7365f2c00a
ionuthodoroaga <ionuthodoroaga@hotmail.com>
2023-08-21 12:04:39 +0300

    feat: finished adding services, added showMessages service

--- angular tutorial tour of heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular tutorial tour of heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,29 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import {HeroService} from '../hero.service';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-constructor(private heroService: HeroService) {}
+  selectedHero?: Hero;
 
-heroes: Hero[] = [];
+  heroes: Hero[] = [];
 
-selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-getHeroes(): void {
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
-ngOnInit(): void {
-  this.getHeroes();
-}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
